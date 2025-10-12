2c68d0dfa87f82c95c2c66de7ef60ef06488b1f1
Ivan <ivanfabianchen@gmail.com>
2025-10-12 20:46:24 +0200

    new test images, image detection with green, purple and erosion

--- Main.py
+++ Main.py
@@ -1,32 +1,35 @@
 import cv2 as cv2
 import numpy as np
 import sys
+import os
 
-img = cv2.imread(cv2.samples.findFile("testFoto1.png"))
-hsvImage = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+img2 = cv2.imread(cv2.samples.findFile("TestImages/Screenshot 2025-10-11 093736.png"))
+img = cv2.imread(cv2.samples.findFile("TestImages/Screenshot 2025-10-08 102026.png"))
 
 if img is None:
     sys.exit("Could not read the image.")
 
+if img2 is None:  
+     sys.exit("Could not read the image.")
 
+
+hsvImage = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+hsvImage2 = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
 
 lower_green = np.array([0,0, 150])
 upper_green = np.array([255, 255, 255])
-
+##(hMin = 112 , sMin = 0, vMin = 0), (hMax = 179 , sMax = 255, vMax = 255)
+lower_purple = np.array([113,0, 0])
+upper_purple = np.array([179, 255, 255])
 mask = cv2.inRange(hsvImage, lower_green, upper_green)
 res = cv2.bitwise_and(img, img, mask=mask)
-
-
+mask2 =cv2.inRange(hsvImage2, lower_purple, upper_purple)
+res1 = cv2.bitwise_and(img2, img2, mask=mask2)
 print(mask)
 hsv_frame = cv2.cvtColor(hsvImage, cv2.COLOR_BGR2HSV)
 
 cv2.imshow("Display window", res)
 k = cv2.waitKey(0)
-
-	
-
-
-
-
-
+cv2.imshow("Display window", res1)
+k = cv2.waitKey(0)
 
--- test.py
+++ test.py
@@ -11,10 +11,13 @@ def load_images_from_folder(folder):
             images.append(img)
     return images
 images = load_images_from_folder("TestImages")
-
+resImages = []
 for image in images:
+    resImages.append(image[0:1400, 0:1900])
     cv2.imshow("Main", image)
     cv2.waitKey()
+images = resImages
+
 lower_green = np.array([0,0, 150])
 upper_green = np.array([255, 255, 255])
 ##(hMin = 112 , sMin = 0, vMin = 0), (hMax = 179 , sMax = 255, vMax = 255)
@@ -22,24 +25,48 @@ lower_purple = np.array([113,0, 0])
 upper_purple = np.array([179, 255, 255])
 
 resultForDisplay = []
-
+displayDifference = []
 for image in images:
+    
     hsvImage = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     greenmask = cv2.inRange(hsvImage, lower_green, upper_green)
     purplemask =cv2.inRange(hsvImage, lower_purple, upper_purple)
     resgreen = cv2.bitwise_and(image, image, mask=greenmask)
     respurple = cv2.bitwise_and(image, image, mask=purplemask)
     finalRes = cv2.bitwise_or(resgreen,respurple)
+
+    kernel = np.ones((5,5),np.uint8)
+    erosion = cv2.erode(finalRes,kernel,iterations = 1)
+
+    displayDifference.extend([image,resgreen,respurple,finalRes,erosion])
+   
     cv2.imshow("Main", resgreen)
-    cv2.waitKey()
+   ## cv2.waitKey()
     cv2.imshow("Main", respurple)
-    cv2.waitKey()
+    ##cv2.waitKey()
     cv2.imshow("Main", finalRes)
+    ##cv2.waitKey()
+    cv2.imshow("Main" ,erosion)
     cv2.waitKey()
     resultForDisplay.append(finalRes)
 
-numpy_horizontal_concat = np.concatenate((finalRes), axis=1)
+im_v = np.concatenate((resultForDisplay[0], resultForDisplay[1]), axis = 1)
+new_width= 300
+new_height = 300
+for i, image in enumerate(displayDifference):
+    displayDifference[i] = cv2.resize(image, (new_width, new_height))
+imdif = np.concatenate((displayDifference[0],displayDifference[1],displayDifference[2],displayDifference[3],displayDifference[4]), axis = 1)
 
-cv2.imshow('Main', numpy_horizontal_concat)
-cv2.imshow('Numpy Vertical', numpy_horizontal_concat)
+imdif1 = np.concatenate((displayDifference[5],displayDifference[6],displayDifference[7],displayDifference[8],displayDifference[9]), axis = 1)
+
+cv2.imshow("window1",imdif)
+cv2.imshow("window2",imdif1)
 cv2.waitKey()
+
+"""cv2.namedWindow("Resized_Window", cv2.WINDOW_NORMAL)
+
+cv2.resizeWindow("Resized_Window", 4000, 4000)
+
+cv2.imshow("Resized_Window", im_v)
+
+cv2.waitKey() """
